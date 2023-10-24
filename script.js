afbc4906b94080cb77388177a69696b315648e46
Gourav Joshi <gourav.joshi2022@vitstudent.ac.in>
2023-10-24 15:41:18 +0530

    second commit

--- script.js
+++ script.js
@@ -1,25 +1,3 @@
-// Initially, hide the BMI Information box.
-document.getElementById("bmiInfo").style.display = "block";
-
-document.getElementById("calculate").addEventListener("click", function () {
-    const age = parseFloat(document.getElementById("age").value);
-    const heightFeet = parseFloat(document.getElementById("heightFeet").value);
-    const heightInches = parseFloat(document.getElementById("heightInches").value);
-    const weight = parseFloat(document.getElementById("weight").value);
-    const result = document.getElementById("result");
-
-    if (isNaN(age) || isNaN(heightFeet) || isNaN(heightInches) || isNaN(weight) || age <= 0 || heightFeet <= 0 || heightInches < 0 || weight <= 0) {
-        result.innerHTML = "Please enter valid age, height, and weight.";
-        document.getElementById("bmiValue").innerHTML = "";
-        document.getElementById("indicatorValue").innerHTML = "Invalid";
-    } else {
-        const heightMeters = ((heightFeet * 12) + heightInches) * 0.0254; // Convert to meters
-        const bmi = weight / (heightMeters * heightMeters);
-        result.innerHTML = `Your BMI is: <span id="bmiValue">${bmi.toFixed(2)}</span>`;
-        displayBMIRange(bmi);
-    }
-});
-
 function displayBMIRange(bmi) {
     const indicator = document.getElementById("indicatorValue");
     if (bmi < 18.5) {
@@ -32,3 +10,21 @@ function displayBMIRange(bmi) {
         indicator.innerHTML = "Obesity";
     }
 }
+
+document.getElementById("calculate").addEventListener("click", function () {
+    const age = parseFloat(document.getElementById("age").value);
+    const heightFeet = parseFloat(document.getElementById("heightFeet").value);
+    const weight = parseFloat(document.getElementById("weight").value);
+    const result = document.getElementById("result");
+
+    if (isNaN(age) || isNaN(heightFeet) || isNaN(weight) || age <= 0 || heightFeet <= 0 || weight <= 0) {
+        result.innerHTML = "Please enter valid age, height, and weight.";
+        document.getElementById("bmiInfo").style.display = "none";
+    } else {
+        const heightMeters = heightFeet * 0.3048; // Convert feet to meters
+        const bmi = weight / (heightMeters * heightMeters);
+        result.innerHTML = `Your BMI is: ${bmi.toFixed(2)}`;
+        displayBMIRange(bmi);
+        document.getElementById("bmiInfo").style.display = "block";
+    }
+});
